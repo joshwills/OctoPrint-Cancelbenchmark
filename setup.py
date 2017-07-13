53c2ae01d74d69cb61a7ebda6594aa1442721e19
joshwills <joshewills@gmail.com>
2017-07-13 09:42:33 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ plugin_author = "Josh"
 plugin_author_email = "you@example.com"
 
 # The plugin's homepage URL. Can be overwritten within OctoPrint's internal data via __plugin_url__ in the plugin module
-plugin_url = "https://github.com/you/OctoPrint-Cancelbenchmark"
+plugin_url = "https://github.com/joshwills/OctoPrint-Cancelbenchmark"
 
 # The plugin's license. Can be overwritten within OctoPrint's internal data via __plugin_license__ in the plugin module
 plugin_license = "AGPLv3"
